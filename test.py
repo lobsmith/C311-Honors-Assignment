526aa31422b1a3c8e61a25a09f376b59cf1747e6
Lorrin Smith <165953346+lobsmith@users.noreply.github.com>
2026-05-04 16:29:26 -0400

    Add test.py

---
+++ test.py
@@ -0,0 +1,37 @@
+x = 10
+y = 3
+z = x + y * 2
+
+print(z)
+
+if (z > 15):
+    print(z)
+else:
+    print(x)
+
+i = 5
+
+while (i > 0):
+    print(i)
+    i = i - 1
+
+a = -5
+b = a * (x + y)
+
+print(b)
+
+if (b != 0):
+    print(b)
+
+'''
+OUTPUT:
+16
+16
+5
+4
+3
+2
+1
+-65
+-65
+'''
